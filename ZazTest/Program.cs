bb20d1e6e2e2ed657bbcec3ff7c232da46090c54
cozz-py <bencozzens13@gmail.com>
2023-02-10 08:31:26 -0500

    initial commit

--- ZazTest/Program.cs
+++ ZazTest/Program.cs
@@ -20,10 +20,23 @@ class Abracadabra
         return false;
     }
 }
-internal class Program
+class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("hello");
+        Pizzazz foxtrot = new Pizzazz() { Zippo = 2 };
+        foxtrot.Bamboo(foxtrot.Zippo);
+        Pizzazz november = new Pizzazz() { Zippo = 3 };
+        Abracadabra tango = new Abracadabra() { Vavavoom = 4 };
+        while (tango.Lala(november.Zippo))
+        {
+            november.Zippo *= -1;
+            november.Bamboo(tango.Vavavoom);
+            foxtrot.Bamboo(november.Zippo);
+            tango.Vavavoom -= foxtrot.Zippo;
+        }
+        Console.WriteLine("november.Zippo = " + november.Zippo);
+        Console.WriteLine("foxtrot.Zippo = " + foxtrot.Zippo);
+        Console.WriteLine("tango.Vavavoom = " + tango.Vavavoom);
     }
 }
