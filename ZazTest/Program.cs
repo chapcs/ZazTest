ff3072d13d176e9d8b1a52bc5f6695f7b106ec62
cozz-py <bencozzens13@gmail.com>
2023-02-10 10:10:55 -0500

    Methods made public to run on C# interactive

--- ZazTest/Program.cs
+++ ZazTest/Program.cs
@@ -1,45 +1,44 @@
-﻿namespace ZazTest
+﻿class Pizzazz
 {
-    class Pizzazz
-    {
-        public int Zippo;
+    public int Zippo;
 
-        public void Bamboo(int eek)
-        {
-            Zippo += eek;
-        }
-    }
-    class Abracadabra
+    public void Bamboo(int eek)
     {
-        public int Vavavoom;
-        public bool Lala(int floq)
-        {
-            if (floq < Vavavoom)
-            {
-                Vavavoom += floq;
-                return true;
-            }
-            return false;
-        }
-    }
-    class Program
-    {
-        private static void Main(string[] args)
-        {
-            Pizzazz foxtrot = new Pizzazz() { Zippo = 2 };
-            foxtrot.Bamboo(foxtrot.Zippo);
-            Pizzazz november = new Pizzazz() { Zippo = 3 };
-            Abracadabra tango = new Abracadabra() { Vavavoom = 4 };
-            while (tango.Lala(november.Zippo))
-            {
-                november.Zippo *= -1;
-                november.Bamboo(tango.Vavavoom);
-                foxtrot.Bamboo(november.Zippo);
-                tango.Vavavoom -= foxtrot.Zippo;
-            }
-            Console.WriteLine("november.Zippo = " + november.Zippo);
-            Console.WriteLine("foxtrot.Zippo = " + foxtrot.Zippo);
-            Console.WriteLine("tango.Vavavoom = " + tango.Vavavoom);
-        }
+        Zippo += eek;
     }
 }
+class Abracadabra
+{
+    public int Vavavoom;
+    public bool Lala(int floq)
+    {
+        if (floq < Vavavoom)
+        {
+            Vavavoom += floq;
+            return true;
+        }
+        return false;
+    }
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Pizzazz foxtrot = new Pizzazz() { Zippo = 2 };
+        foxtrot.Bamboo(foxtrot.Zippo);
+        Pizzazz november = new Pizzazz() { Zippo = 3 };
+        Abracadabra tango = new Abracadabra() { Vavavoom = 4 };
+        while (tango.Lala(november.Zippo))
+        {
+            november.Zippo *= -1;
+            november.Bamboo(tango.Vavavoom);
+            foxtrot.Bamboo(november.Zippo);
+            tango.Vavavoom -= foxtrot.Zippo;
+        }
+        Console.WriteLine("november.Zippo = " + november.Zippo);
+        Console.WriteLine("foxtrot.Zippo = " + foxtrot.Zippo);
+        Console.WriteLine("tango.Vavavoom = " + tango.Vavavoom);
+    }
+}
+//can copy and paste code into developer command prompt or C# interactive window
+//Program.Main(new string[] {}) to run, make sure all methods/classes are public
