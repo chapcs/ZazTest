b0ec18fbde8a3543312bca91f16083bda3f81c3a
cozz-py <bencozzens13@gmail.com>
2023-02-10 08:26:55 -0500

    Add project files.

---
+++ ZazTest/Program.cs
@@ -0,0 +1,29 @@
+﻿
+class Pizzazz
+{
+    public int Zippo;
+    public void Bamboo(int eek)
+    {
+        Zippo += eek;
+    }
+}
+class Abracadabra
+{
+    public int Vavavoom;
+    public bool Lala(int floq)
+    {
+        if (floq < Vavavoom)
+        {
+            Vavavoom += floq;
+            return true;
+        }
+        return false;
+    }
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("hello");
+    }
+}
